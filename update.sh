d44c5d54eea0138de77a4ca4f11dc3f04dc2e1ec
ilham Deneri <ilhamdeneri@gmail.com>
2015-08-11 21:40:38 +0300

    first commit

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Get Repository
+git pull origin master
+
+# Update Database
+app/console doctrine:schema:update --force
+
+# Remove Cache
+rm -rf app/cache/prod/*
+rm -rf app/logs/*
+
+# Clear Doctrine Cache
+app/console doctrine:cache:clear-query
+app/console doctrine:cache:clear-result
+
+# Assets Install
+app/console assets:install web
+
+# Assetic Dump
+app/console assetic:dump
